69a146fa291efe45ea034790494f7a42de90c004
MohamedASC2020 <mdrame1109@gmail.com>
2020-08-03 22:49:25 -0400

    third commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+for( i=0; i<5; i++){
+var myImg = document.querySelector("#skyline");
+        var currWidth = myImg.clientWidth;
+        var currHeight = myImg.clientHeight;
+
+let textPlacement = currHeight -20
+var amText = document.querySelector('#amText')
+amText.style.top = textPlacement + 'px'
+} 
