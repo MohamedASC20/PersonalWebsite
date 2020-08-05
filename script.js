2214a7e1b321511ba72ee4433f7594edb8ff9553
MohamedASC2020 <mdrame1109@gmail.com>
2020-08-05 02:38:26 -0400

    contactForm

--- script.js
+++ script.js
@@ -1,11 +1,66 @@
+/*
+const submitButton = document.getElementById('submit');
+submitButton.addEventListener('click', submitForm);
 
 
-for( i=0; i<5; i++){
-var myImg = document.querySelector("#skyline");
-        var currWidth = myImg.clientWidth;
-        var currHeight = myImg.clientHeight;
+function submitForm() {
 
-let textPlacement = currHeight -20
-var amText = document.querySelector('#amText')
-amText.style.top = textPlacement + 'px'
-} 
+ 
+ event.preventDefault();
+ const formname = document.getElementById('fname').value
+ const formemail = document.getElementById('email').value
+ const formsubject = document.getElementById('title').value
+ const formmessage = document.getElementById('message').value
+
+ let allinfo = []
+
+ allinfo.push({
+     name : formname,
+     email: formemail,
+     subject: formsubject,
+     message: formmessage,
+ })
+
+ console.log(allinfo);
+
+
+ 
+}*/
+const messageSubmitted = document.getElementById('mS');
+messageSubmitted.style.display = 'none';
+const fnameElement = document.getElementById("fname");
+const lnameElement = document.getElementById("lname");
+const emailElement = document.getElementById("email");
+const titleElement = document.getElementById("title");
+const messageElement = document.getElementById("message");
+const button = document.getElementById("submitButton");
+button.addEventListener("click",updateDB);
+
+
+//Set database object here
+let db = firebase.database().ref();
+
+/**
+ * Updates the database with the username and message.
+ */
+function updateDB(event){
+    event.preventDefault();
+    const name    =     titleElement.value + ' ' + fnameElement.value + ' ' + lnameElement.value;
+    const message         = messageElement.value;
+
+    fnameElement.value = "";
+    lnameElement.value = "";
+    emailElement.value = "";
+    messageElement.value  = "";
+ messageSubmitted.style.display = 'block';
+    console.log(name + " : " + message);
+
+    //Update database here
+    let userInput = {
+        Name : name,
+        Message: message
+    }
+
+    db.push(userInput);
+
+}
